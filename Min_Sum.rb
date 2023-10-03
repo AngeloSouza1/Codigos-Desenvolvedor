f920b32eac5036e2b74393ea59a8f2a1c6ffce39
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-10-02 21:27:09 -0300

    feat: Inserção código Min_Sum

---
+++ Min_Sum.rb
@@ -0,0 +1,12 @@
+def min_sum(arr)
+    min_val = arr.min
+    return min_val * (arr.length - 1)
+  end
+  
+  # Programa principal
+  arr = [3, 6, 2, 8, 7, 5]
+  
+#   puts min_sum(arr)
+  
+
+  #Em Ruby, usamos o método min para encontrar o valor mínimo no array, em vez de min_element.
