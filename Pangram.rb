5a10c8ff3c641949641ddd8f0cf33e96f5938194
Angelo Souza <104176076+AngeloSouza1@users.noreply.github.com>
2023-10-04 18:55:57 -0300

    refactor: Realizando refatoração do código
    Nesta versão, usei unless em vez de if para verificar se caracteres_faltantes não está vazio, o que torna o código mais legível. Além disso, usei caracteres_faltantes.any? para verificar se há caracteres faltantes em vez de caracteres_faltantes.empty?. Isso deixa o código mais claro em relação à intenção de verificar se há qualquer caractere faltante.

--- Pangram.rb
+++ Pangram.rb
@@ -1,11 +1,13 @@
-
 def caracteres_ausentes_para_pangram(string)
-    alfabeto = ('a'..'z').to_a
-    caracteres_faltantes = alfabeto - string.downcase.chars.uniq
-    caracteres_faltantes.empty? ? 'A string já é um pangram.' : caracteres_faltantes.join('')
-  end
-  
-  # Exemplo de uso:
-  string = "a rápida raposa marrom pula sobre o cão preguiçoso"
+  alfabeto = ('a'..'z').to_a
+  caracteres_faltantes = alfabeto - string.downcase.chars.uniq
+  return 'A string já é um pangram.' unless caracteres_faltantes.any?
+  caracteres_faltantes.join('')
+end
+
+# Exemplo de uso:
+string = "The quick brown fox jumps over the lazy do"
+puts caracteres_ausentes_para_pangram(string)
+puts "-------------------------"
+string = "The quick brown fox jumps over the lazy dog"
   puts caracteres_ausentes_para_pangram(string)
-  
