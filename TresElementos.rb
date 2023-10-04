97d5a2dd32d0c146d870f9b19fe89e325ff1aba5
Angelo Souza <104176076+AngeloSouza1@users.noreply.github.com>
2023-10-04 18:59:16 -0300

    refactor: Realizando refatoração do código
    Utilizei a função sort para ordenar o array em ordem decrescente e, 
    em seguida, usamos [0..2] para pegar os três primeiros elementos (os maiores). Essa abordagem 
    é mais concisa e legível.

--- TresElementos.rb
+++ TresElementos.rb
@@ -1,36 +1,8 @@
 def encontrar_tres_maiores_elementos(arr)
-  # Remove elementos duplicados da matriz
-  arr = arr.uniq
-
-  # Se a matriz tiver menos de três elementos, preencha com -Infinity
-  while arr.length < 3
-    arr << -Float::INFINITY
-  end
-
-  # Inicializando as variáveis para armazenar os três maiores elementos
-  primeiro_maior = -Float::INFINITY
-  segundo_maior = -Float::INFINITY
-  terceiro_maior = -Float::INFINITY
-
-  # Percorrendo a matriz para encontrar os três maiores elementos
-  arr.each do |elemento|
-    if elemento > primeiro_maior
-      # Atualiza os elementos em ordem, mantendo o rastreamento dos três maiores
-      terceiro_maior = segundo_maior
-      segundo_maior = primeiro_maior
-      primeiro_maior = elemento
-    elsif elemento > segundo_maior
-      terceiro_maior = segundo_maior
-      segundo_maior = elemento
-    elsif elemento > terceiro_maior
-      terceiro_maior = elemento
-    end
-  end
-
-  # Retorna os três maiores elementos encontrados
-  [primeiro_maior, segundo_maior, terceiro_maior]
+  arr.sort.reverse[0..2]
 end
 
-
-
-  
+# Exemplo de uso:
+arr = [10, 4, 3, 50, 23, 90]
+resultados = encontrar_tres_maiores_elementos(arr)
+puts "Os três maiores elementos são: #{resultados.join(', ')}"
