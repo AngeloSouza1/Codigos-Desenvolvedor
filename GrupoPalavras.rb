4d52e4fe5731db47f075cb7d0bed8493dfb9cbcf
Angelo Souza <104176076+AngeloSouza1@users.noreply.github.com>
2023-10-04 18:49:37 -0300

    refactor: Realizando refatoração do código
    Nesta refatoração:
    Utilizei Hash.new { |hash, key| hash[key] = [] } para criar um hash onde os valores são inicializados automaticamente como arrays vazios, eliminando a necessidade de verificar se a chave já existe.
    Simplifiquei o cálculo do conjunto de caracteres da palavra, ordenando os caracteres antes de juntá-los.
    O restante do código permaneceu semelhante, agrupando as palavras com base no conjunto de caracteres e, em seguida, imprimindo-as.
    Isso torna o código mais eficiente e legível.

--- GrupoPalavras.rb
+++ GrupoPalavras.rb
@@ -1,35 +1,21 @@
-
 def agrupa_palavras_com_mesmo_conjunto_caracteres(words)
-    # Crie um hash para mapear conjuntos de caracteres exclusivos para palavras
-    conjunto_palavras = {}
-  
-    # Passo 1: Iterar por todas as palavras
-    words.each do |word|
-      # Passo 2: Criar um conjunto de caracteres exclusivos da palavra
-      conjunto = word.chars.uniq.sort.join
-  
-      # Passo 3: Verificar se o conjunto já existe no hash
-      if conjunto_palavras.include?(conjunto)
-        # Se existir, adicione a palavra ao conjunto existente
-        conjunto_palavras[conjunto] << word
-      else
-        # Se não existir, crie um novo conjunto com a palavra
-        conjunto_palavras[conjunto] = [word]
-      end
-    end
-  
-    # Passo 4: Imprimir as palavras com o mesmo conjunto de caracteres agrupadas
-    conjunto_palavras.each_value do |palavras|
-      puts palavras.join(", ")
-    end
+  conjunto_palavras = Hash.new { |hash, key| hash[key] = [] }
+
+  words.each do |word|
+    conjunto = word.chars.sort.join
+    conjunto_palavras[conjunto] << word
   end
-  
-  # Exemplo de uso
-  words = ["may", "student", "students", "dog",
-           "studentssess", "god", "cat", "act",
-           "tab", "bat", "flow", "wolf", "lambs",
-           "amy", "yam", "balms", "looped", 
-           "poodle"]
 
+  conjunto_palavras.each_value do |palavras|
+    puts palavras.join(", ")
+  end
+end
 
- agrupa_palavras_com_mesmo_conjunto_caracteres(words)
+# Exemplo de uso
+words = ["may", "student", "students", "dog",
+         "studentssess", "god", "cat", "act",
+         "tab", "bat", "flow", "wolf", "lambs",
+         "amy", "yam", "balms", "looped", 
+         "poodle"]
+
+agrupa_palavras_com_mesmo_conjunto_caracteres(words)
